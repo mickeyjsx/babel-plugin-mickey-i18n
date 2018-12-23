7ca4f052c8325ff69848bcc54c0df23cecd22e6c
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2018-12-24 01:52:11 +0800

    update hashcode method

--- src/getTextId.js
+++ src/getTextId.js
@@ -1,21 +1,5 @@
-/* eslint-disable no-bitwise */
-
-function hashcode(str) {
-  let hash = 0
-
-  if (!str || str.length === 0) {
-    return hash
-  }
-
-  for (let i = 0; i < str.length; i += 1) {
-    const char = str.charCodeAt(i)
-    hash = ((hash << 5) - hash) + char
-    hash &= hash
-  }
-
-  return hash
-}
+import stringHash from '@sindresorhus/string-hash'
 
 export default function (text) {
-  return hashcode(text)
+  return stringHash(text)
 }
